8d2d0cc819dfa8add1d8b902b84cc112b6952342
remoko-source <muhammadreykhandinoval@gmail.com>
2026-05-25 20:14:44 +0700

    Update Mp-3.py

--- Mp-3.py
+++ Mp-3.py
@@ -2,6 +2,8 @@
 #Simple choice game
 
 import os
+game = bool
+current = 0
 inventory = {
 "gold" : 10,
 "potion" : 5,
@@ -18,6 +20,14 @@ mental = {
 "HP" : 100,
 "power": 11
 }
+
+def lanjut() :
+ cek= input("Berhenti? y/t: ")  
+ if cek == "y" :
+  return False
+ os.system("cls")
+ return True
+ 
 def man1():
  print("+10 Gold")
  inventory["gold"] += 10
@@ -27,6 +37,7 @@ def man1():
  inventory["item"].append("Venomshank")
  input()
  os.system("cls")
+ return
   
 def lihat():
  pass
@@ -36,15 +47,24 @@ def man2():
   print("Kamu menang!")
   print("+25 EXP")
   mental["exp"]+= 25
+  return
  else :
   print("Anda kalah")
   print("-25 HP")
   mental["HP"] -= 25
- input()
- os.system("cls")
+  return
 
-def awal(): 
- print("Kamu masuk gua")
+def mon():
+ inp = input("Check stats dan inventory? 1/2 = ")
+ if inp == "1" :
+  os.system("cls")
+  menu()
+  return
+ else :
+  os.system("cls")
+  return
+ 
+def lol() :
  pilihan = input("Mau kiri atau kanan? ")
  if pilihan == "kiri":
   print("Ketemu harta")
@@ -53,12 +73,28 @@ def awal():
   print("Ketemu monster")
   man2()
 
-def lanjut() :
- cek= input("Berhenti? y/t: ")  
- if cek == "y" :
-  return False
- return True
+def hutan():
+ print("Kamu masuk Hutan")
+ lol()
+
+def gua():
+ print("Kamu masuk Gua")
+
+def kastil():
+ print("Kamu masuk Kastil")
+ pass
  
+def padang_pasir():
+ print("Kamu masuk Padang Pasir")
+ pass
+ 
+area = [
+hutan,
+kastil,
+gua,
+padang_pasir
+]
+
 def menu():
  print("1. Cek item")
  print("2. Cek peralatan")
@@ -66,25 +102,30 @@ def menu():
  print("4. Skip ")
  cok = input("Mana yang mau anda lakukan? : ")
  if cok == "1" :
+  print()
   for key, value in inventory.items():
    if key != "item":
     print(key, ":", value)
+  input()
  elif cok == "2":
+  print()
   for key, value in inventory.items():
    if key == "item":
     for i in range(len(inventory["item"])) :
      print(i+1,".",value[i])
+  input()
 
  elif cok == "3" :
   for key, value in mental.items() :
    print(key, ":", value)
  elif cok == "4" :
-  pass
+  os.system("cls")
+  return
  else :
   print("Tidak valid")
  
-while True:  
-  awal()
-  menu()
-  if not lanjut():
-   break
+while game and current < len(area):
+  area[current]()
+  mon()
+  game = lanjut()
+  current += 1
