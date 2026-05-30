b7c904183cb68d8d3949bdc7377209d780c3e069
remoko-source <muhammadreykhandinoval@gmail.com>
2026-05-30 20:32:14 +0700

    Update Mp-6.py

---
+++ Mp-6.py
@@ -0,0 +1,19 @@
+data = {
+"Reykhandi"  :{
+"umur" : 17,
+"kota" : "DKI Jakarta"
+},
+
+"Athala"  :{
+"umur" : 16,
+"kota" : "Jakarta"
+},
+
+"Azral"  :{
+"umur" : 18,
+"kota" : "Depok"
+}
+}
+
+print("Data Berhasil dimuat!!")
+print("Jumlah Siswa: ",len(data))
